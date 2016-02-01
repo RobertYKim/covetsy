661eb19e2331656198b85f84056050b4fb22147f
RobertYKim <rob.y.kim@gmail.com>
2016-02-01 01:13:50 -0500

    Add featured seller and info to hero.

--- frontend/components/hero.jsx
+++ frontend/components/hero.jsx
@@ -7,6 +7,16 @@ var Hero = React.createClass({
       <div className="hero">
         <div className="hero-body">
           <h1>Shop directly from people around the world.</h1>
+          <div className="hero-space"></div>
+          <div className="hero-seller">
+            <div className="hero-seller-picture"></div>
+            <div className="hero-seller-info">
+              <p className="name-store">
+                Robert of <a href="#" onClick={this.handleClick}>CraftCoding</a>
+              </p>
+              <p className="location">New York, New York</p>
+            </div>
+          </div>
         </div>
         <div className="hero-overlay">
         </div>
