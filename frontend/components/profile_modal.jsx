de39f98f0fd8850112da69deaf287d01edbd63b3
RobertYKim <rob.y.kim@gmail.com>
2016-01-31 19:56:04 -0500

    Add current user info to profile modal.

--- frontend/components/profile_modal.jsx
+++ frontend/components/profile_modal.jsx
@@ -1,5 +1,6 @@
 var React = require('react'),
-    ProfileModalActions = require('../actions/profile_modal_actions');
+    ProfileModalActions = require('../actions/profile_modal_actions'),
+    CurrentUserStore = require('../stores/current_user_store');
 
 var ProfileModal = React.createClass({
   handleClick: function (event) {
@@ -10,9 +11,39 @@ var ProfileModal = React.createClass({
   },
 
   render: function () {
+    var profileImage;
+    profileImage = <span className="fa fa-user fa-4x"></span>;
+
+    var profileName,
+        currentUser,
+        identifier;
+    currentUser = CurrentUserStore.currentUser();
+    if (currentUser.first_name && currentUser.last_name) {
+      identifier = currentUser.first_name + " " + currentUser.last_name;
+    } else if (currentUser.first_name) {
+      identifier = currentUser.first_name;
+    } else if (currentUser.last_name) {
+      identifier = currentUser.last_name;
+    } else {
+      identifier = currentUser.username;
+    }
+    profileName = <h4>{identifier}</h4>;
+
+    var profileViewButton;
+    profileViewButton =
+      <a href="#" className="profile-modal-button">
+        View profile
+      </a>;
+
     return (
-      <div className="profile-modal" onClick={this.handleClick}>
-        I am a ProfileModal
+      <div className="profile-modal group" onClick={this.handleClick}>
+        <div className="profile-modal-image">
+          {profileImage}
+        </div>
+        <div className="profile-modal-info">
+          {profileName}
+          {profileViewButton}
+        </div>
       </div>
     );
   }
