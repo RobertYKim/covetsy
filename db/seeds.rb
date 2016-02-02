df54262cfee561dfc0e15ccfb799def6f3d1be5c
RobertYKim <rob.y.kim@gmail.com>
2016-02-02 10:18:31 -0500

    Seed guest user account.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Users
+User.destroy_all
+guest = User.create!(
+  first_name: "Dev",
+  last_name: "Eloper",
+  username: "guest",
+  email: "guest@gmail.com",
+  password: "password"
+)
