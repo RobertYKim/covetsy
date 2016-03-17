6fe03160ca751528ffb07b78f1f2e8d75db2493b
RobertYKim <rob.y.kim@gmail.com>
2016-03-16 22:09:18 -0400

    Add content and styling to checkout modal component.

--- frontend/components/checkout_modal.jsx
+++ frontend/components/checkout_modal.jsx
@@ -2,9 +2,49 @@ var React = require('react'),
     CheckoutModalActions = require('../actions/checkout_modal_actions');
 
 var CheckoutModal = React.createClass({
+  handleClick: function (event) {
+    if (event.target.className === "overlay") {
+      CheckoutModalActions.hideCheckoutModal();
+    }
+  },
+
   render: function () {
     return (
-      <div>This is a checkout modal</div>
+      <div className="overlay" onClick={this.handleClick}>
+        <div className="checkout-modal">
+          <h3>Thanks for visiting covEtsy!</h3>
+          <p>
+            Unfortunately you can't buy any of these items, but if you enjoyed
+            this app, check out more about me and my work below!
+          </p>
+          <div className="checkout-links group">
+            <div className="checkout-github">
+              <a href="https://www.github.com/RobertYKim" target="_blank">
+                <div className="github-icon"></div>
+              </a>
+              <a href="https://www.github.com/RobertYKim" target="_blank">
+                Github
+              </a>
+            </div>
+            <div className="checkout-linkedin">
+              <a href="https://www.linkedin.com/in/RobYKim" target="_blank">
+                <div className="linkedin-icon"></div>
+              </a>
+              <a href="https://www.linkedin.com/in/RobYKim" target="_blank">
+                LinkedIn
+              </a>
+            </div>
+            <div className="checkout-home">
+              <a href="http://www.robkim.io" target="_blank">
+                <div className="home-icon"></div>
+              </a>
+              <a href="http://www.robkim.io" target="_blank">
+                Website
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 });
